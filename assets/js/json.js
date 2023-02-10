8a6d77a44ccd99ba8c5618c0c73d007940d2cf47
utchi-kanna <118425953+utchi-kanna@users.noreply.github.com>
2023-02-10 08:43:05 +0530

    10-feb-2023

---
+++ assets/js/json.js
@@ -0,0 +1,15 @@
+// let productContainers = [...document.querySelectorAll(".menu")];
+// let nxtBtn = [...document.querySelectorAll(".next")];
+// let prevBtn = [...document.querySelectorAll(".prev")];
+
+// productContainers.forEach((item, i) => {
+//   let containerDimensions = item.getBoundingClientRect();
+//   let containerWidth = containerDimensions.width;
+
+//   nxtBtn[i].addEventListener("click", () => {
+//     item.scrollLeft += containerWidth;
+//   });
+//   prevBtn[i].addEventListener("click", () => {
+//     item.scrollLeft -= containerWidth;
+//   });
+// });
